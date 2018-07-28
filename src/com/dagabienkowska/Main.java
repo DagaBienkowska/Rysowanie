5a11851be08555021507d97c3939540f939943e5
Dagmara Bieńkowska <froggyzor@gmail.com>
2018-07-28 11:58:53 +0200

    choinka

--- src/com/dagabienkowska/Main.java
+++ src/com/dagabienkowska/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //linia
         int n = 10;
         for (int i = 0; i < n; i++) {
             System.out.print("*");
         }
 
+        //caly kwadrat
         System.out.println("\n");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -17,6 +19,7 @@ public class Main {
             System.out.println();
         }
 
+        //pusty kwadrat
         System.out.println("\n");
         for (int i = 0; i < n; i++) {
             if (i == 0 || i == n - 1) {
@@ -35,6 +38,7 @@ public class Main {
             System.out.println();
         }
 
+        //trojkat prostokatny
         System.out.println("\n");
         for (int i = 0; i < n; i++) {
             for (int j = n - i; j < n; j++) {
@@ -44,6 +48,7 @@ public class Main {
             System.out.println();
         }
 
+        //trojkat rownoramienny
         System.out.println("\n");
         n = 11;
         for (int i = 0; i < n; i++) {
@@ -51,6 +56,26 @@ public class Main {
                 System.out.print(" ");
             }
             for (int k = 1 + i*2; k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1 + i*2; k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n/2-1; i++){
+            for (int j = 0; j < n/2+1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < n; k++) {
                 System.out.print("*");
             }
             System.out.println();
