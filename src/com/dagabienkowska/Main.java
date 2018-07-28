61d6e4f994ca9c63cde57ad49b8d059a705eb6d7
Dagmara Bieńkowska <froggyzor@gmail.com>
2018-07-28 11:03:03 +0200

    linia i kwadrat

--- src/com/dagabienkowska/Main.java
+++ src/com/dagabienkowska/Main.java
@@ -4,9 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 10; i++){
+        int n = 10;
+        for (int i = 0; i < n; i++){
             System.out.print("*");
         }
 
+        System.out.println("\n");
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print("**");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <)
+        }
     }
 }
