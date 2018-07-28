aa08e7ad34042151a928914a81f84343716a12e7
Dagmara Bieńkowska <froggyzor@gmail.com>
2018-07-28 11:42:10 +0200

    trojkat rownoramienny

--- src/com/dagabienkowska/Main.java
+++ src/com/dagabienkowska/Main.java
@@ -5,27 +5,27 @@ public class Main {
     public static void main(String[] args) {
 
         int n = 10;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             System.out.print("*");
         }
 
         System.out.println("\n");
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("**");
             }
             System.out.println();
         }
 
         System.out.println("\n");
-        for (int i = 0; i < n; i++){
-            if (i == 0 || i == n-1){
-                for (int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i == n - 1) {
+                for (int j = 0; j < n; j++) {
                     System.out.print("*");
                 }
             } else {
-                for (int j = 0; j < n; j++){
-                    if (j == 0 || j == n-1) {
+                for (int j = 0; j < n; j++) {
+                    if (j == 0 || j == n - 1) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
@@ -36,8 +36,21 @@ public class Main {
         }
 
         System.out.println("\n");
-        for (int i = 0; i < n; i++){
-            for (int j = n-i; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j < n; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        n = 11;
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1 + i*2; k > 0; k--){
                 System.out.print("*");
             }
             System.out.println();
