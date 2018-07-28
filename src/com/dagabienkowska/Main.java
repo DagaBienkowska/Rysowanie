68efcbe28d5e2eeafa4c995a1f4a1b55de353343
Dagmara Bieńkowska <froggyzor@gmail.com>
2018-07-28 11:19:38 +0200

    trojkat

--- src/com/dagabienkowska/Main.java
+++ src/com/dagabienkowska/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         System.out.println("\n");
         for (int i = 0; i < n; i++){
-            if (i == 0 || i == 9){
+            if (i == 0 || i == n-1){
                 for (int j = 0; j < n; j++){
                     System.out.print("*");
                 }
@@ -31,6 +31,14 @@ public class Main {
                         System.out.print(" ");
                     }
                 }
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        for (int i = 0; i < n; i++){
+            for (int j = n-i; j < n; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
